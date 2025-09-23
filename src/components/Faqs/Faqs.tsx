d5242f2916c210672fb2c2d1e04eae407f11e770
Rashid Abdurakhimov <rashid.abdurakhimov@gmail.com>
2025-09-23 14:57:46 +0300

    fix

--- src/components/Faqs/Faqs.tsx
+++ src/components/Faqs/Faqs.tsx
@@ -18,6 +18,43 @@ export const Faqs: FC = () => {
         </div>
         <Image src={MaskBanner} alt="mask-banner" className={styles.FaqsBannerCardMask} />
       </div>
+      <div className={styles.FaqsQuestions}>
+        <h2 className={styles.FaqsQuestionsTitle}>General</h2>
+        <div className={styles.FaqsQuestionsItem}>
+          <h3 className={styles.FaqsQuestionsItemTitle}>
+            01. How do I book a trip on your website?
+          </h3>
+          <p>-</p>
+        </div>
+        <hr />
+        <p className={styles.FaqsQuestionsText}>
+          Aptent taciti sociosqu ad litora torquent per conubia nostra, per inci only Integer purus
+          onthis felis non aliquam.Mauris nec just vitae ann auctor tol euismod sit amet non ipsul
+          growing this
+        </p>
+        <div className={styles.FaqsQuestionsItem}>
+          <h3 className={styles.FaqsQuestionsItemTitle}>02. What payment methods do you accept?</h3>
+          <p>+</p>
+        </div>
+        <hr />
+        <div className={styles.FaqsQuestionsItem}>
+          <h3 className={styles.FaqsQuestionsItemTitle}>
+            03. Can I make changes to my reservation after booking?
+          </h3>
+          <p>+</p>
+        </div>
+        <hr />
+        <div className={styles.FaqsQuestionsItem}>
+          <h3 className={styles.FaqsQuestionsItemTitle}>04. What is your cancellation policy?</h3>
+          <p>+</p>
+        </div>
+        <hr />
+        <div className={styles.FaqsQuestionsItem}>
+          <h3 className={styles.FaqsQuestionsItemTitle}>05. Do You Offer Group Booking Discounts?</h3>
+          <p>+</p>
+        </div>
+        <hr />
+      </div>
     </div>
   );
 };
