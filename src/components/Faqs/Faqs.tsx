6750f8747820bf9ae6c7e55dfa9818c7ec2069df
DESKTOP-S5BN3DV\Rashid <rashid.abdurakhimov@gmail.com>
2025-09-23 23:08:39 +0300

    WIP FAQS

--- src/components/Faqs/Faqs.tsx
+++ src/components/Faqs/Faqs.tsx
@@ -1,12 +1,75 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState } from 'react';
 import styles from './Faqs.module.scss';
 import Image from 'next/image';
 import MaskBanner from '@/assets/images/MaskBanner.png';
 import { DefaultButton } from '@/components/common/DefaultButton/DefaultButton';
+import titleMask from '@/assets/images/titleMask.png';
+
+const faqs = [
+  {
+    id: 1,
+    question: '01. How do I book a trip on your website?',
+    answer:
+      'Aptent taciti sociosqu ad litora torquent per conubia nostra, per inci only Integer purus onthis felis non aliquam...',
+  },
+  {
+    id: 2,
+    question: '02. What payment methods do you accept?',
+    answer: 'We accept Visa, MasterCard, PayPal and other common payment methods.',
+  },
+  {
+    id: 3,
+    question: '03. Can I make changes to my reservation after booking?',
+    answer: 'Yes, you can change your reservation by contacting support at least 24h before.',
+  },
+  {
+    id: 4,
+    question: '04. What is your cancellation policy?',
+    answer: 'Free cancellation up to 48h before departure.',
+  },
+];
+
+const tips = [
+  {
+    id: 1,
+    question: '01. What Is Your Payment Schedule And Process?',
+    answer:
+      'Aptent taciti sociosqu ad litora torquent per conubia nostra, per inci only Integer purus onthis felis non aliquam. Mauris nec just vitae ann auctor tol euismod sit amet non ipsul growing this',
+  },
+  {
+    id: 2,
+    question: '02. Are There Any Additional Fees Or Surcharges?',
+    answer: 'We accept Visa, MasterCard, PayPal and other common payment methods.',
+  },
+  {
+    id: 3,
+    question: '03. Can I Transfer My Reservation To Another Person?',
+    answer: 'Yes, you can change your reservation by contacting support at least 24h before.',
+  },
+  {
+    id: 4,
+    question: '04. Can I Request A Private Tour For My Group?',
+    answer: 'Free cancellation up to 48h before departure.',
+  },
+  {
+    id: 5,
+    question: '05. Do You Offer Special Rates For Group Bookings?',
+    answer: 'Free cancellation up to 48h before departure.',
+  },
+];
 
 export const Faqs: FC = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const toggleFaq = (id: number) => {
+    setActiveIndex(activeIndex === id ? null : id);
+  };
+
   return (
     <div className={styles.Faqs}>
+      {/* Банер */}
       <div className={styles.FaqsBannerCard}>
         <div className={styles.FaqsBannerCardContent}>
           <div>
@@ -18,42 +81,61 @@ export const Faqs: FC = () => {
         </div>
         <Image src={MaskBanner} alt="mask-banner" className={styles.FaqsBannerCardMask} />
       </div>
+
+      {/* Питання */}
       <div className={styles.FaqsQuestions}>
-        <h2 className={styles.FaqsQuestionsTitle}>General</h2>
-        <div className={styles.FaqsQuestionsItem}>
-          <h3 className={styles.FaqsQuestionsItemTitle}>
-            01. How do I book a trip on your website?
-          </h3>
-          <p>-</p>
+        <div className={styles.FaqsQuestionsTop}>
+          <h2 className={styles.FaqsQuestionsTopTitle}>General</h2>
+          <Image src={titleMask} alt="title-mask" />
         </div>
-        <hr />
-        <p className={styles.FaqsQuestionsText}>
-          Aptent taciti sociosqu ad litora torquent per conubia nostra, per inci only Integer purus
-          onthis felis non aliquam.Mauris nec just vitae ann auctor tol euismod sit amet non ipsul
-          growing this
-        </p>
-        <div className={styles.FaqsQuestionsItem}>
-          <h3 className={styles.FaqsQuestionsItemTitle}>02. What payment methods do you accept?</h3>
-          <p>+</p>
+        {faqs.map((faq) => (
+          <div key={faq.id}>
+            <div
+              className={styles.FaqsQuestionsItem}
+              onClick={() => toggleFaq(faq.id)}
+              style={{ cursor: 'pointer' }}
+            >
+              <h3 className={styles.FaqsQuestionsItemTitle}>{faq.question}</h3>
+              <p>{activeIndex === faq.id ? '-' : '+'}</p>
+            </div>
+            {activeIndex === faq.id && (
+              <p
+                className={`${styles.FaqsQuestionsText} ${
+                  activeIndex === faq.id ? styles.isOpen : ''
+                }`}
+              >
+                {faq.answer}
+              </p>
+            )}
+            <hr />
+          </div>
+        ))}
+        <div className={styles.FaqsQuestionsTopTips}>
+          <h2 className={styles.FaqsQuestionsTopTitle}>Travel Tips</h2>
+          <Image src={titleMask} alt="title-mask" />
         </div>
-        <hr />
-        <div className={styles.FaqsQuestionsItem}>
-          <h3 className={styles.FaqsQuestionsItemTitle}>
-            03. Can I make changes to my reservation after booking?
-          </h3>
-          <p>+</p>
-        </div>
-        <hr />
-        <div className={styles.FaqsQuestionsItem}>
-          <h3 className={styles.FaqsQuestionsItemTitle}>04. What is your cancellation policy?</h3>
-          <p>+</p>
-        </div>
-        <hr />
-        <div className={styles.FaqsQuestionsItem}>
-          <h3 className={styles.FaqsQuestionsItemTitle}>05. Do You Offer Group Booking Discounts?</h3>
-          <p>+</p>
-        </div>
-        <hr />
+        {tips.map((faq) => (
+          <div key={faq.id}>
+            <div
+              className={styles.FaqsQuestionsItem}
+              onClick={() => toggleFaq(faq.id)}
+              style={{ cursor: 'pointer' }}
+            >
+              <h3 className={styles.FaqsQuestionsItemTitle}>{faq.question}</h3>
+              <p>{activeIndex === faq.id ? '-' : '+'}</p>
+            </div>
+            {activeIndex === faq.id && (
+              <p
+                className={`${styles.FaqsQuestionsText} ${
+                  activeIndex === faq.id ? styles.isOpen : ''
+                }`}
+              >
+                {faq.answer}
+              </p>
+            )}
+            <hr />
+          </div>
+        ))}
       </div>
     </div>
   );
